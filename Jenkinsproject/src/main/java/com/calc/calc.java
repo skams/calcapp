046b97caca0015585769d69a0975604e32511b36
ramya <jramya118@gmail.com>
2016-08-11 13:18:32 +0530

    updated sub func

--- Jenkinsproject/src/main/java/com/calc/calc.java
+++ Jenkinsproject/src/main/java/com/calc/calc.java
@@ -4,6 +4,11 @@ public class calc {
 	public double add(double a, double b) {
 		double result = a + b;
 		return result;
+	}
+
+	public double sub(double a, double b) {
+		double result = a - b;
+		return result;
 
 	}
 
